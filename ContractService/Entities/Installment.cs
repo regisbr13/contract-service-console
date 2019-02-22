99f6993d5dc0d9b447b0a41aca8347f3e04c90c7
Regis Lima <regislima1391@gmail.com>
2019-02-22 12:03:02 -0300

    Criadas as entidades Contract e Installment

---
+++ ContractService/Entities/Installment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContractService.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Quantia { get; private set; }
+
+        public Installment(DateTime dueDate, double quantia)
+        {
+            DueDate = dueDate;
+            Quantia = quantia;
+        }
+    }
+}
