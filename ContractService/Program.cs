23d9f7ed0e812b8107d95fd5e97ca6b8b66a8259
Regis Lima <regislima1391@gmail.com>
2019-02-22 16:19:53 -0300

    Implementado tratamento de erros de sistema

--- ContractService/Program.cs
+++ ContractService/Program.cs
@@ -9,27 +9,35 @@ namespace ContractService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter contract data");
-            Console.Write("Number: ");
-            int number = int.Parse(Console.ReadLine());
-            Console.Write("Date (dd/MM/yyyy): ");
-            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
-            Console.Write("Contract value: ");
-            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Contract contract = new Contract(number, date, value);
-
-            Console.Write("Enter number of installments: ");
-            int months = int.Parse(Console.ReadLine());
-
-            ContractProcessing service = new ContractProcessing(new PaypalService());
-            service.ProcessContract(contract, months);
-
-            Console.WriteLine("Installments:");
-            foreach(Installment installment in contract.installments)
+            try
             {
-                Console.WriteLine(installment);
+                Console.WriteLine("Enter contract data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Date (dd/MM/yyyy): ");
+                DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                Console.Write("Contract value: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Contract contract = new Contract(number, date, value);
+
+                Console.Write("Enter number of installments: ");
+                int months = int.Parse(Console.ReadLine());
+
+                ContractProcessing service = new ContractProcessing(new PaypalService());
+                service.ProcessContract(contract, months);
+
+                Console.WriteLine("Installments:");
+                foreach (Installment installment in contract.installments)
+                {
+                    Console.WriteLine(installment);
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro inesperado: " + e.Message);
+            }
+            
         }
     }
 }
